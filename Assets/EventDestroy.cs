61c10142d9ebbad63400063f37158bbe151a71da
Oliver Phillip Roer <phazyck@gmail.com>
2015-03-03 17:29:00 +0100

    Players now have tails.

---
+++ Assets/EventDestroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventDestroy : MonoBehaviour {
+
+    public void EventDestroyObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Tail.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tail : MonoBehaviour {
+
+    public float interval;
+
+    private float timeSinceLastDrop;
+
+    public GameObject tailPrefab;
+
+    public Transform tailsParent; 
+
+	// Use this for initialization
+	void Start () {
+
+        timeSinceLastDrop = interval;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeSinceLastDrop += Time.deltaTime;
+
+        if (timeSinceLastDrop > interval)
+        {
+            timeSinceLastDrop += interval;
+
+            GameObject g = (GameObject) Instantiate(tailPrefab, this.transform.position, this.transform.rotation);
+            g.transform.parent = tailsParent;
+        }
+	
+	}
+}
