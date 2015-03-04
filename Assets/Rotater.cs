c0c407f07f07f1ef60ee95875cc21d496ac3a407
Søren L. Vange <DCoin@users.noreply.github.com>
2015-03-05 00:40:35 +0100

    Added InControl and simple (stupid) controller support

--- Assets/Rotater.cs
+++ Assets/Rotater.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
-public class Rotater : MonoBehaviour {
+public class Rotater : MonoBehaviour
+{
 
-    public KeyCode rotateLeft;
-    public KeyCode rotateRight;
-
-    public float rotateSpeed;
+	public KeyCode rotateLeft;
+	public KeyCode rotateRight;
+	public float rotateSpeed;
+	public int rotateAxis;
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-        Vector3 rot = rotateSpeed * Vector3.forward;
+		Vector3 rot = rotateSpeed * Vector3.forward;
 
-        if (Input.GetKey(rotateLeft))
-        {
-            transform.Rotate(-rot);
-        }
+		var device = InputManager.ActiveDevice;
+		var axis = rotateAxis == 0 ? device.LeftStick : rotateAxis == 1 ? device.RightStick : device.DPad;
 
-        if (Input.GetKey(rotateRight))
-        {
-            transform.Rotate(rot);
-        }
+		if (axis.Vector.sqrMagnitude > 0.1) {
+			transform.localRotation = Quaternion.FromToRotation (Vector3.up, axis.Vector);
+		} else {
+			if (Input.GetKey (rotateLeft)) {
+				transform.Rotate (-rot);
+			}
 
+			if (Input.GetKey (rotateRight)) {
+				transform.Rotate (rot);
+			}
+		}
 	}
 }
