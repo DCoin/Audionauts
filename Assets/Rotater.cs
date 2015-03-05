c5096577218c75f429666ec209a0020df3da2d84
Oliver Phillip Roer <phazyck@gmail.com>
2015-03-05 11:26:21 +0100

    Added smoother movement.

--- Assets/Rotater.cs
+++ Assets/Rotater.cs
@@ -7,28 +7,39 @@ public class Rotater : MonoBehaviour
 
 	public KeyCode rotateLeft;
 	public KeyCode rotateRight;
-	public float rotateSpeed;
 	public int rotateAxis;
-	
+	public float rotateAcceleration;
+	public float rotateMaxSpeed = 3;
+
+	private float rotateSpeed = 0;
+
 	// Update is called once per frame
 	void Update ()
 	{
-
-		Vector3 rot = rotateSpeed * Vector3.forward;
-
 		var device = InputManager.ActiveDevice;
 		var axis = rotateAxis == 0 ? device.LeftStick : rotateAxis == 1 ? device.RightStick : device.DPad;
 
 		if (axis.Vector.sqrMagnitude > 0.1) {
 			transform.localRotation = Quaternion.FromToRotation (Vector3.up, axis.Vector);
 		} else {
-			if (Input.GetKey (rotateLeft)) {
-				transform.Rotate (-rot);
-			}
 
-			if (Input.GetKey (rotateRight)) {
-				transform.Rotate (rot);
+			if (Input.GetKey (rotateLeft)) {
+				rotateSpeed = Mathf.Max(-rotateMaxSpeed, rotateSpeed - rotateAcceleration);
+			} else if (Input.GetKey (rotateRight)) {
+				rotateSpeed = Mathf.Min (rotateMaxSpeed, rotateSpeed + rotateAcceleration);
+			} else {
+				
+				if(rotateSpeed > 0f) {
+					rotateSpeed = Mathf.Max(0f, rotateSpeed - rotateAcceleration);
+				} else {
+					rotateSpeed = Mathf.Min(0f, rotateSpeed + rotateAcceleration);
+				}
 			}
+			
+			Vector3 rot = rotateSpeed * Vector3.forward;
+
+
+			transform.Rotate(rot);
 		}
 	}
 }
