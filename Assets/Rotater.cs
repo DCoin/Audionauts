0df0fed5820919e8820a4fa6266bc4887118b9ee
Søren L. Vange <DCoin@users.noreply.github.com>
2015-03-10 00:53:37 +0100

    Added proper gravity and controller support

--- Assets/Rotater.cs
+++ Assets/Rotater.cs
@@ -2,44 +2,39 @@
 using System.Collections;
 using InControl;
 
-public class Rotater : MonoBehaviour
+public class Rotater : HamsterPhysic
 {
-
-	public KeyCode rotateLeft;
-	public KeyCode rotateRight;
 	public int rotateAxis;
 	public float rotateAcceleration;
-	public float rotateMaxSpeed = 3;
+	public float maxVelocity = 10;
+	public float friction = 0.1f;
 
-	private float rotateSpeed = 0;
+	public HamsterPhysic wheel;
+
+	public override void FixedUpdate ()
+	{
+		if (wheel != null) {
+			// Calculate gravity
+			var v = transform.rotation * Vector3.up; // Vector where x and y is cos and sin of the angle
+			var f = Mass * g * v.x;
+			angularVelocity += f / Mass * Time.deltaTime;
+
+			var combinedForce = angularVelocity * Mass + wheel.angularVelocity * wheel.Mass;
+			var combinedVelocity = combinedForce / (Mass + wheel.Mass);
+			angularVelocity += (combinedVelocity - angularVelocity) * friction;
+			wheel.angularVelocity += (combinedVelocity - wheel.angularVelocity) * friction;
+		}
+		base.FixedUpdate();
+	}
 
 	// Update is called once per frame
 	void Update ()
 	{
 		var device = InputManager.ActiveDevice;
 		var axis = rotateAxis == 0 ? device.LeftStick : rotateAxis == 1 ? device.RightStick : device.DPad;
-
-		if (axis.Vector.sqrMagnitude > 0.1) {
-			transform.localRotation = Quaternion.FromToRotation (Vector3.up, axis.Vector);
-		} else {
-
-			if (Input.GetKey (rotateLeft)) {
-				rotateSpeed = Mathf.Max(-rotateMaxSpeed, rotateSpeed - rotateAcceleration);
-			} else if (Input.GetKey (rotateRight)) {
-				rotateSpeed = Mathf.Min (rotateMaxSpeed, rotateSpeed + rotateAcceleration);
-			} else {
-				
-				if(rotateSpeed > 0f) {
-					rotateSpeed = Mathf.Max(0f, rotateSpeed - rotateAcceleration);
-				} else {
-					rotateSpeed = Mathf.Min(0f, rotateSpeed + rotateAcceleration);
-				}
-			}
-			
-			Vector3 rot = rotateSpeed * Vector3.forward;
-
-
-			transform.Rotate(rot);
-		}
+		if (axis.X > 0.1f || axis.X < -0.1f) {
+			if (axis.X < 0 && angularVelocity > -maxVelocity || axis.X > 0 && angularVelocity < maxVelocity)
+				angularVelocity += axis.X * rotateAcceleration * Time.deltaTime;
+		} 
 	}
 }
