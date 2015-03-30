baf2f9d76f900f4360b48a375d1626f4124d866b
Oliver Phillip Roer <phazyck@gmail.com>
2015-03-31 01:38:43 +0200

    Added GrooveManager and a Canvas for fading out as you lose your groove.

---
+++ Assets/GrooveFader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Image))]
+public class GrooveFader : MonoBehaviour {
+
+	Image im;
+	GrooveManager gm;
+
+
+	// Use this for initialization
+	void Start () {
+
+		im = this.GetComponent<Image>();
+		gm = GrooveManager.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		Color c = im.color;
+		c.a = 1f - gm.GroovePCT;
+		im.color = c;
+	
+	}
+}
---
+++ Assets/GrooveManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GrooveManager : MonoBehaviour {
+
+	public float maxGroove;
+
+	public float depletionRate;
+
+	private float currentGroove;
+
+	public float GroovePCT {
+		get {
+			return currentGroove / maxGroove;
+		}
+	}
+	
+	public static GrooveManager Instance { get; private set; }
+	
+	void Start () {
+
+		currentGroove = maxGroove;
+
+		if (Instance == null) {
+			Instance = this;
+		} else {
+			Debug.LogError ("A scene should only have one GrooveManager");
+		}
+	}
+
+	void Update () {
+
+		currentGroove -= Time.deltaTime * depletionRate;
+		if(currentGroove < 0f) {
+			currentGroove = 0f;
+		}
+	}
+
+}
