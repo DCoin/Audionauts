3d26e4874d41fc5793da731895cf512d04474323
Oliver Phillip Roer <phazyck@gmail.com>
2015-03-31 13:43:33 +0200

    Mirrorers.

---
+++ Assets/Mirrorer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Mirrorer : MonoBehaviour {
+
+	public Transform source;
+
+
+	public float angle;
+
+	// Update is called once per frame
+	void LateUpdate () {
+
+		var v = source.transform.position;
+		v = Quaternion.AngleAxis(angle, Vector3.forward) * v;
+
+		//v.x *= -1f;
+		//v.y *= -1f;
+		transform.position = v;
+	
+	}
+}
