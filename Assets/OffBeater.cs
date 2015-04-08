56d8b268e95af92934a3c7bb715498c3ba3683ce
Runeweisekofoed <Rune.weise.kofoed@gmail.com>
2015-04-08 20:35:19 +0200

    Bass started + more

---
+++ Assets/OffBeater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffBeater : MonoBehaviour {
+
+
+	public int A;
+	public int B;
+
+	void OnValidate() {
+
+		Vector3 pos = transform.localPosition;
+
+		float a = A;
+		float b = B;
+
+		pos.z = 4.0f * a / b;
+
+		transform.localPosition = pos;
+
+	}
+
+
+}
