a8ddbcc1323c3944f46fbfefdd5e823331f3f42a
Oliver Phillip Roer <phazyck@gmail.com>
2015-04-21 19:12:39 +0200

    Player bobbing reimplemented.

---
+++ Assets/Scripts/PlayerBobber.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class PlayerBobber : MonoBehaviour {
+
+        public AnimationCurve Animation;
+
+        private MusicManager mm;
+
+        private Vector3 localScale;
+
+        // Use this for initialization
+        void Start () {
+	
+            mm = MusicManager.Instance;
+
+            localScale = transform.localScale;
+
+        }
+	
+        // Update is called once per frame
+        void Update () {
+
+            var time = mm.SmoothBeatsPlayed;
+            time -= (int) time;
+
+            var value = Animation.Evaluate(time);
+
+            transform.localScale = value*localScale;
+
+        }
+
+    }
+}
