40b20b858eca0db508b80f400b90e6d1c9fdaabd
Søren L. Vange <DCoin@users.noreply.github.com>
2015-05-02 19:31:10 +0200

    Proposing controller changes

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -15,6 +15,17 @@ namespace Assets.Scripts
 
         public int Controller;
 
+		public bool UseAcceleration = true;
+		public float acc = 12;
+
+		public bool UseTarget = false;
+		public AnimationCurve ClosenessDeceleration;
+
+		public bool UseFidelity = false;
+		public AnimationCurve Fidelity;
+
+		private Vector2 velocity = Vector2.zero;
+
         private static TwoAxisInputControl GetAxis(InputDevice device, AxisSource source)
         {
 
@@ -62,8 +73,19 @@ namespace Assets.Scripts
             var v = axis.Vector;
             v.x *= flip.x;
             v.y *= flip.y;
+
+			if (UseFidelity) v = v.normalized * Fidelity.Evaluate(v.magnitude);
+
+			if (UseTarget) {
+				var target = v.normalized * Radius;
+				var direction = target - (Vector2)transform.localPosition;
+				v = direction.normalized * v.magnitude * ClosenessDeceleration.Evaluate(direction.magnitude);
+			}
+
+			if (UseAcceleration) velocity = Vector2.Lerp(velocity, v*Speed, acc * Time.deltaTime);
+			else velocity = v * Speed;
             
-            transform.Translate(v * Speed 
+            transform.Translate(velocity 
                 * Time.deltaTime
                 , Space.World);
 
