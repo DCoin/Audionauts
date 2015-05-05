28d55288e03bd62b3c3daa5ee625e5e84426e9a6
Oliver Phillip Roer <phazyck@gmail.com>
2015-05-05 16:22:20 +0200

    Menu actions now play sounds.

---
+++ Assets/ActionPlaySound.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ActionPlaySound : MenuAction {
+
+    public AudioClip[] Sounds;
+
+    private AudioSource source;
+
+    private System.Random rnd;
+
+    public override void Execute() {
+
+        int i = rnd.Next(0, Sounds.Length);
+
+        AudioClip clip = Sounds[i];
+
+        source.clip = clip;
+
+        source.Play();
+
+    }
+
+    void Start() {
+
+        source = GetComponent<AudioSource>();
+
+        rnd = new System.Random();
+
+    }
+
+}
