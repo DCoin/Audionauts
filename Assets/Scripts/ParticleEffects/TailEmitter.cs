062b8174c12f448d192bd5551afcf949add65218
Oliver Phillip Roer <phazyck@gmail.com>
2015-05-07 18:21:51 +0200

    Experimenting with tails.

--- Assets/Scripts/ParticleEffects/TailEmitter.cs
+++ Assets/Scripts/ParticleEffects/TailEmitter.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Assets.Scripts;
 
 public class TailEmitter : MonoBehaviour {
@@ -8,6 +7,8 @@ public class TailEmitter : MonoBehaviour {
 	public int EmissionRate = 1000;
 	public float Radius = .1f;
 	public float Speed = 40;
+    public float Rotation = 0f;
+    private float totalRot = 0f;
 	//public bool interpolate;
 
 	ParticleSystem _particleSystem;
@@ -20,6 +21,7 @@ public class TailEmitter : MonoBehaviour {
 	}
 
 	void Start () {
+
 		_particleSystem = GetComponent<ParticleSystem>();
 		lastPosition = Player.position;
 
@@ -32,15 +34,43 @@ public class TailEmitter : MonoBehaviour {
 
 		var emitCount = EmissionRate * Time.deltaTime;
 		for (int i = 0; i < emitCount; i++) {
-			var velocity = Vector3.forward * -Speed;
+			var pVelocity = Vector3.forward * -Speed;
 			var fraction = i/emitCount;
-			/*if (interpolate)*/ _particleSystem.Emit(Vector3.Lerp(localPlayerPosition, lastPosition + velocity * Time.deltaTime, fraction) + (Vector3)(Radius * Random.insideUnitCircle),
-			                                          velocity,
-			                                          _particleSystem.startSize + playerModel.localScale.y,
-			                                          Mathf.Lerp(_particleSystem.startLifetime, _particleSystem.startLifetime + Time.deltaTime, fraction),
-			                                          _particleSystem.startColor);
+
+		    //var part = new ParticleSystem.Particle();
+		    var pPosition = Vector3.Lerp(localPlayerPosition, lastPosition + pVelocity*Time.deltaTime, fraction) +
+		                    (Vector3) (Radius*Random.insideUnitCircle);
+		    var pSize = _particleSystem.startSize + playerModel.localScale.y;
+		    var pLifeTime = Mathf.Lerp(
+		        _particleSystem.startLifetime,
+		        _particleSystem.startLifetime + Time.deltaTime,
+		        fraction);
+		    var pColor = _particleSystem.startColor;
+
+			/*if (interpolate)*/
+		    _particleSystem.Emit(pPosition, pVelocity, pSize, pLifeTime, pColor);
 			//else _particleSystem.Emit(Vector3.Lerp(lastPosition, Player.position, ((float)i)/EmissionRate), Vector3.zero, _particleSystem.startSize, _particleSystem.startLifetime, _particleSystem.startColor);
 		}
 		lastPosition = localPlayerPosition;
+
+	    int pCount = _particleSystem.particleCount;
+        ParticleSystem.Particle[] pParticles = new ParticleSystem.Particle[pCount];
+
+
+	    _particleSystem.GetParticles(pParticles);
+
+	    totalRot += Rotation * Mathf.PI * 2f;
+
+	    for (int i = 0; i < pCount; ++i) {
+
+	        if (pParticles[i].rotation == 0f) {
+	            pParticles[i].rotation = totalRot;
+	        }
+
+            //pParticles[i].rotation += Rotation;
+
+	    }
+
+        _particleSystem.SetParticles(pParticles, pCount);
 	}
 }
