5e9006b60774838e7b6c3c0de0dce58b7c2af5f8
Oliver Phillip Roer <phazyck@gmail.com>
2015-05-07 12:59:32 +0200

    Tail tweaks in menu.

--- Assets/Scripts/ParticleEffects/TailEmitter.cs
+++ Assets/Scripts/ParticleEffects/TailEmitter.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
 
 public class TailEmitter : MonoBehaviour {
 
@@ -12,6 +13,8 @@ public class TailEmitter : MonoBehaviour {
 	ParticleSystem _particleSystem;
 	Vector3 lastPosition;
 
+    private Transform playerModel;
+
 	Vector3 localPlayerPosition {
 		get { return Player.localPosition; }
 	}
@@ -19,16 +22,21 @@ public class TailEmitter : MonoBehaviour {
 	void Start () {
 		_particleSystem = GetComponent<ParticleSystem>();
 		lastPosition = Player.position;
+
+	    PlayerBobber b = Player.GetComponentInChildren<PlayerBobber>();
+	    playerModel = b.transform;
+
 	}
 
 	void LateUpdate() {
+
 		var emitCount = EmissionRate * Time.deltaTime;
 		for (int i = 0; i < emitCount; i++) {
 			var velocity = Vector3.forward * -Speed;
 			var fraction = i/emitCount;
 			/*if (interpolate)*/ _particleSystem.Emit(Vector3.Lerp(localPlayerPosition, lastPosition + velocity * Time.deltaTime, fraction) + (Vector3)(Radius * Random.insideUnitCircle),
 			                                          velocity,
-			                                          _particleSystem.startSize,
+			                                          _particleSystem.startSize + playerModel.localScale.y,
 			                                          Mathf.Lerp(_particleSystem.startLifetime, _particleSystem.startLifetime + Time.deltaTime, fraction),
 			                                          _particleSystem.startColor);
 			//else _particleSystem.Emit(Vector3.Lerp(lastPosition, Player.position, ((float)i)/EmissionRate), Vector3.zero, _particleSystem.startSize, _particleSystem.startLifetime, _particleSystem.startColor);
