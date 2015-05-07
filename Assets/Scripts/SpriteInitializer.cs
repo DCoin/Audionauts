c82224bd1695884add1ec1f9636fc84deddb6d64
Oliver Phillip Roer <phazyck@gmail.com>
2015-05-07 21:24:25 +0200

    Ring tweaks.

--- Assets/Scripts/SpriteInitializer.cs
+++ Assets/Scripts/SpriteInitializer.cs
@@ -4,12 +4,19 @@ namespace Assets.Scripts {
     [RequireComponent(typeof(SpriteRenderer))]
     public class SpriteInitializer : MonoBehaviour {
 
+        public bool customAlpha = false;
+        public float alpha;
+
         // Use this for initialization
         void Start () {
 
             var n = transform.GetComponentInParent<Note>();
             var r = GetComponent<SpriteRenderer>();
-            r.color = n.CurrentColor;
+            var c = n.CurrentColor;
+            if(customAlpha)
+                c.a = alpha;
+            r.color = c;
+
         }
 	
     }
