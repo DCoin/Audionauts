7357e7687fdb06d73eff113b8b965cb25e7d5edc
Oliver Phillip Roer <phazyck@gmail.com>
2015-05-08 03:40:25 +0200

    Splash screen.

---
+++ Assets/TimedFader.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Managers;
+using UnityEngine;
+
+public class TimedFader : MonoBehaviour {
+
+    public float duration;
+
+    public string scene;
+
+    void LoadScene() {
+
+        Application.LoadLevel(scene);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	    var d = duration;
+
+	    duration -= Time.deltaTime;
+
+	    if (duration <= 0 && d > 0) {
+	        FadeManager.Instance.EndScene(LoadScene);
+	    } 
+
+	}
+}
